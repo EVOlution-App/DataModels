cdacfe65e7d17cf087df74d4c0f25e45d9f4f8c1
Thiago Holanda <unnamedd@gmail.com>
2018-03-10 15:24:40 +0100

    removed unnecessary items

--- Sources/DataModelsCore/Extensions/String+Extension.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public extension String {
-    func trimming() -> String {
-        return self.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-}
--- Sources/DataModelsCore/Models/Bug.swift
+++ Sources/DataModelsCore/Models/Bug.swift
@@ -14,40 +14,6 @@ public final class Bug: Model {
     public var radar: String?
     public var assignee: String?
     public var resolution: String?
-    
-//    init?(_ object: Models.Bug) {
-//        if let value = object.id, value.trimming() != "" {
-//            id = value
-//        }
-//
-//        if let value = object["status"], value.trimming() != "" {
-//            status = value
-//        }
-//
-//        if let value = object["updated"], value.trimming() != "" {
-//            updated = value
-//        }
-//
-//        if let value = object["title"], value.trimming() != "" {
-//            updated = value
-//        }
-//
-//        if let value = object["link"], value.trimming() != "" {
-//            updated = value
-//        }
-//
-//        if let value = object["radar"], value.trimming() != "" {
-//            updated = value
-//        }
-//
-//        if let value = object["assignee"], value.trimming() != "" {
-//            updated = value
-//        }
-//
-//        if let value = object["resolution"], value.trimming() != "" {
-//            resolution = value
-//        }
-//    }
 }
 
 extension Bug: CustomStringConvertible {
