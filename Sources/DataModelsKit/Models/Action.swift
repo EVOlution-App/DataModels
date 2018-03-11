244440b1ba1a11ef155980f2e6d71c221ec8a9f2
Thiago Holanda <unnamedd@gmail.com>
2018-03-11 18:52:27 +0100

    added new properties to Action model

--- Sources/DataModelsKit/Models/Action.swift
+++ Sources/DataModelsKit/Models/Action.swift
@@ -17,14 +17,26 @@ public enum ActionType: String, Codable {
     case bugInProgress
 }
 
+public enum RelationType: String, Codable {
+    case bug
+    case person
+    case status
+    case proposal
+}
+
 public final class Action: Model {
     public var _id = ObjectId()
     public var _createdAt = Date()
     public var _updatedAt = Date()
     
     public var user: Reference<Person>?
-    public var type: ActionType?
-    public var targetID: ObjectId?
+
+    public var actionID: ObjectId?
+    public var actionType: ActionType?
+    
+    public var relationID: ObjectId?
+    public var relationType: RelationType?
+    
     public var note: String?
     
     public init() {}
