60cbfb946a3e5cb6802515d83ce1bf9a2b48a826
Thiago Holanda <unnamedd@gmail.com>
2018-03-18 12:52:31 +0100

    add MQ.Message model

---
+++ Sources/DataModelsKit/MQ.Message.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct MQ {
+    public struct Message: Codable {
+        public var user: String?
+        
+        public var actionID: String?
+        public var actionType: ActionType?
+        
+        public var relationID: String?
+        public var relationType: RelationType?
+    }
+}
