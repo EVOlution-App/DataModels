994a0527bc25516435979942ab302e35c7f5f86a
Thiago Holanda <unnamedd@gmail.com>
2018-04-11 03:17:40 +0200

    added tag model

--- Sources/DataModelsKit/Tag.swift
+++ Sources/DataModelsKit/Tag.swift
@@ -4,8 +4,9 @@ import Meow
 public final class Tag: Model {
     public var _id = ObjectId()
     
-    public var key: String?
-    public var value: String?
-    
+    public var name: String?
+    public var createdAt: Date?
+    public var updatedAt: Date?
+
     public init() {}
 }
