570b31f9f5b49b204aacc0cac62931a4545b9da0
Thiago Holanda <unnamedd@gmail.com>
2018-04-11 03:17:07 +0200

    added track model

--- Sources/DataModelsKit/Track.swift
+++ Sources/DataModelsKit/Track.swift
@@ -1,1 +1,12 @@
 import Foundation
+import Meow
+
+public final class Track: Model {
+    public var _id = ObjectId()
+    
+    public var notification : Reference<Notification>?
+    public var user         : Reference<User>?
+    public var createdAt    : Date?
+    
+    public init() {}
+}
