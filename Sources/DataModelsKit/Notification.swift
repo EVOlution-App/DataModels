0c66df0e0992b16c2c40aa3516bb590d1da87fbb
Thiago Holanda <unnamedd@gmail.com>
2018-04-11 03:16:57 +0200

    added notification model

--- Sources/DataModelsKit/Notification.swift
+++ Sources/DataModelsKit/Notification.swift
@@ -1,1 +1,17 @@
 import Foundation
+import Meow
+
+public final class Notification: Model {
+    public var _id = ObjectId()
+    
+    public var title    : String?
+    public var subtitle : String?
+    public var body     : String?
+    public var delivered: Bool = false
+    public var category : ActionType?
+    public var payload  : Document?
+    
+    public var createdAt: Date?
+    
+    public init() {}
+}
