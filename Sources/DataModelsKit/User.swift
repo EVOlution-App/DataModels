1e71d0d3735b1412e8c9d21871d794782b19fb37
Thiago Holanda <unnamedd@gmail.com>
2018-04-11 03:18:29 +0200

    changed property tags from list of tags to reference tag

--- Sources/DataModelsKit/User.swift
+++ Sources/DataModelsKit/User.swift
@@ -2,12 +2,12 @@ import Foundation
 import Meow
 
 public final class User: Model {
-    public var _id = ObjectId()
-    public var createdAt: Date?
-    public var updatedAt: Date?
+    public var _id          = ObjectId()
+    public var createdAt    : Date?
+    public var updatedAt    : Date?
     
-    public var identifier: String?
-    public var tags:       [Tag]?
+    public var identifier   : String?
+    public var tags         : [Reference<Tag>]?
     
     public init() {}
 }
