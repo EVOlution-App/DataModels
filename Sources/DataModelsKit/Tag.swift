cb7f4f7ab857c47fdc2953e2efdf25687d31282b
Thiago Holanda <unnamedd@gmail.com>
2018-04-13 22:52:50 +0200

    added new property to Tag model

--- Sources/DataModelsKit/Tag.swift
+++ Sources/DataModelsKit/Tag.swift
@@ -5,6 +5,7 @@ public final class Tag: Model {
     public var _id = ObjectId()
     
     public var name: String?
+    public var identifier: ActionType?
     public var createdAt: Date?
     public var updatedAt: Date?
 
