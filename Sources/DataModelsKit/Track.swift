881cde2b6a5e7de52a253b573e0fd2e4d472b16d
Thiago Holanda <unnamedd@gmail.com>
2018-04-15 00:19:15 +0200

    added source to Track model

--- Sources/DataModelsKit/Track.swift
+++ Sources/DataModelsKit/Track.swift
@@ -1,12 +1,19 @@
 import Foundation
 import Meow
 
+public enum SourceTrack: String, Codable {
+    case ios
+    case macos
+    case safari
+    case chrome
+}
+
 public final class Track: Model {
     public var _id = ObjectId()
     
     public var notification : Reference<Notification>?
     public var user         : Reference<User>?
-    public var source       : String?
+    public var source       : SourceTrack?
     public var createdAt    : Date?
     
     public init() {}
