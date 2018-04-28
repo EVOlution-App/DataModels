a2541aff29ab3e7967c44659d3ff8906bdc854d4
Thiago Holanda <unnamedd@gmail.com>
2018-04-28 16:25:16 +0200

    renamed property on OneSignal model

--- Sources/DataModelsKit/OneSignal.swift
+++ Sources/DataModelsKit/OneSignal.swift
@@ -4,7 +4,7 @@ import Meow
 public final class OneSignal: Model {
     public var _id = ObjectId()
     
-    public var onesignal    : String?
+    public var key          : String?
     public var recipients   : Int?
     
     public init() {}
