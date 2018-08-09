0971d83b8c6c1403596e5c106cd57bf627caf1d5
Thiago Holanda <unnamedd@gmail.com>
2018-08-09 13:35:36 +0100

    update Package.swift to use Meow from branch 'master/2.0'

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["DataModelsKit"]),
         ],
     dependencies: [
-        .package(url: "https://github.com/OpenKitten/Meow.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/OpenKitten/Meow.git", .branch("master/2.0")),
     ],
     targets: [
         .target(
